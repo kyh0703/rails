d514cd97a317c37ac177b479ba6b5678eff88326
김연호 <kyh0703@bridgetec.co.kr>
2026-01-19 17:57:46 +0900

    feat: add auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
+  resource :sign_up
+
+  namespace :settings do
+    resource :email, only: [ :show, :update ]
+    resource :password, only: [ :show, :update ]
+    resource :profile, only: [ :show, :update ]
+    resource :user, only: [ :show, :destroy ]
+
+    root to: redirect("/settings/profile")
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
